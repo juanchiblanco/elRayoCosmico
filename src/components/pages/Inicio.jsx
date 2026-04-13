976196f503e91e2ad8b62481e5155433c3278cda
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2026-04-13 16:20:26 -0300

    agregue imagenes al carousel en dispositivos md y lg

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,42 +1,43 @@
 import React from "react";
 import logoERC from "../../assets/fotosERC/ERClogo.png";
-import descoordinadoTapa from "../../assets/fotosERC/descoordinadoTapa.jpg";
-import NuncaMeFuiAlbum from "../../assets/fotosERC/NuncaMeFuiAlbum.png";
+import imgCarousel1 from "../../assets/fotosERC/imgCarousel1.png";
+import imgCarousel2 from "../../assets/fotosERC/imgCarousel2.png";
+import imgCarousel3 from "../../assets/fotosERC/imgCarousel3.png";
 import { Carousel } from "react-bootstrap";
 
 const Inicio = () => {
   return (
     <section>
-      <article className="d-flex justify-content-center container-fluid lineaSeparadora">
+      <Carousel className="carousel">
+        <Carousel.Item>
+          <img
+            className="d-block img-carousel img-fluid"
+            src={imgCarousel1}
+            alt="nunca me fui album"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block img-carousel img-fluid"
+            src={imgCarousel2}
+            alt="plataformas"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block img-carousel img-fluid"
+            src={imgCarousel3}
+            alt="Contacto"
+          />
+        </Carousel.Item>
+      </Carousel>
+      <article className="d-flex justify-content-center container-fluid lineaSeparadora mt-4">
         <img
           src={logoERC}
           alt="El Rayo Cosmico"
-          className="img-fluid logoERC mb-0"
+          className="img-fluid logoERC"
         />
       </article>
-      <Carousel>
-        <Carousel.Item>
-            <img
-              className="img-fluid"
-              src={descoordinadoTapa}
-              alt="Descoordinado"
-            />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-              className="img-fluid"
-              src={NuncaMeFuiAlbum}
-              alt="Descoordinado"
-            />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-              className="img-fluid"
-              src={descoordinadoTapa}
-              alt="Descoordinado"
-            />
-        </Carousel.Item>
-      </Carousel>
     </section>
   );
 };

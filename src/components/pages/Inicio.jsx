a9f20f23454e25c62b7f591e03a2a03e65a16395
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2026-04-17 21:51:43 -0300

    agregue carrusel con imagene para moviles pequenios

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -3,12 +3,15 @@ import logoERC from "../../assets/fotosERC/ERClogo.png";
 import imgCarousel1 from "../../assets/fotosERC/imgCarousel1.png";
 import imgCarousel2 from "../../assets/fotosERC/imgCarousel2.png";
 import imgCarousel3 from "../../assets/fotosERC/imgCarousel3.png";
+import imgCarousel1Movil from "../../assets/fotosERC/imgCarousel1Movil.png";
+import imgCarousel2Movil from "../../assets/fotosERC/imgCarousel2Movil.png";
+import imgCarousel3Movil from "../../assets/fotosERC/imgCarousel3Movil.png";
 import { Carousel } from "react-bootstrap";
 
 const Inicio = () => {
   return (
     <section>
-      <Carousel className="carousel">
+      <Carousel className="carousel d-none d-md-block">
         <Carousel.Item>
           <img
             className="d-block img-carousel img-fluid"
@@ -27,6 +30,29 @@ const Inicio = () => {
           <img
             className="d-block img-carousel img-fluid"
             src={imgCarousel3}
+            alt="Contacto"
+          />
+        </Carousel.Item>
+      </Carousel>
+      <Carousel className="carousel d-block d-md-none">
+        <Carousel.Item>
+          <img
+            className="d-block img-carousel img-fluid"
+            src={imgCarousel1Movil}
+            alt="nunca me fui album"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block img-carousel img-fluid"
+            src={imgCarousel2Movil}
+            alt="plataformas"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block img-carousel img-fluid"
+            src={imgCarousel3Movil}
             alt="Contacto"
           />
         </Carousel.Item>

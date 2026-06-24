4d998f002c87cd8f2d697fd5c39b16d305745c1f
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2026-06-24 09:34:19 -0300

    avance en card album

--- src/components/pages/Canciones.jsx
+++ src/components/pages/Canciones.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
+import { Container, Card, Row, Col, Button, Alert } from "react-bootstrap";
 import descoordinadoTapa from "../../assets/fotosERC/descoordinadoTapa.jpg";
 import elCieloTapa from "../../assets/fotosERC/elCieloTapa.png";
 import porLasNochesTapa from "../../assets/fotosERC/porLasNochesTapa.png";
@@ -13,24 +13,39 @@ const Canciones = () => {
       <section className="container-fluid mb-4">
         <article className="container">
           <h5 className="ms-5 fs-3 my-3">Albums</h5>
-          <div className="d-flex justify-content-center gap-2">
-            <Card className="card-canciones d-flex flex-column shadow">
-              <Card.Img
-                src={NuncaMeFuiAlbum}
-                alt="Nunca Me Fui Arte de tapa"
-                className="img-fluid"
-              />
-              <Card.Body className="d-flex flex-column">
-                <Card.Title className="arapey fs-1">Nunca me fui</Card.Title>
-                <Card.Text className="text-truncate"></Card.Text>
-                <Card.Text className="raleway">
-                  Descripción del album
-                </Card.Text>
-                <Card.Text className="ms-auto">
-                  <strong>
-                    Escuchar<i className="bi bi-arrow-right"></i>
-                  </strong>
-                </Card.Text>
+          <div className="row justify-content-center">
+            <Card className="col-12 shadow">
+              <Card.Body>
+                <Row>
+                  <Col md={6} lg={5}>
+                    <div className="d-flex justify-content-center">
+                      <img
+                        src={NuncaMeFuiAlbum}
+                        alt="NuncaMeFui Album Tapa"
+                        className="img-fluid"
+                      />
+                    </div>
+                  </Col>
+
+                  {/* Columna de Información */}
+                  <Col md={6} lg={7}>
+                    <div className="mt-3 mt-md-0 d-flex flex-column align-content-around">
+                      {/* Categoría */}
+                      <div className="d-flex justify-content-between align-items-center mb-3">
+                        <span className="badge">hola</span>
+                      </div>
+
+                      {/* Título */}
+                      <h2 className="arapey mb-3 fs-1">Nunca Me Fui</h2>
+                      <p className="text-muted mb-4">31 min | Febrero 2026 </p>
+                      <div>
+                        <ul>
+                            <li>hola</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
               </Card.Body>
             </Card>
           </div>
@@ -38,12 +53,12 @@ const Canciones = () => {
         <hr />
         <article className="container">
           <h5 className="ms-5 fs-3 my-3">Singles</h5>
-          <div className="d-flex justify-content-center gap-2">
-            <Card className="card-canciones d-flex flex-column shadow">
+          <div className="row justify-content-center gap-2">
+            <Card className="d-flex flex-column shadow col-12 col-md-5">
               <Card.Img
                 src={descoordinadoTapa}
                 alt="Descoordinado Arte de tapa"
-                className="img-fluid"
+                className="img-fluid my-3"
               />
               <Card.Body className="d-flex flex-column">
                 <Card.Title className="arapey fs-1">Descoordinado</Card.Title>
@@ -58,11 +73,11 @@ const Canciones = () => {
                 </Card.Text>
               </Card.Body>
             </Card>
-            <Card className="card-canciones d-flex flex-column shadow">
+            <Card className="d-flex flex-column shadow col-12 col-md-5">
               <Card.Img
                 src={elCieloTapa}
                 alt="El Cielo Arte de tapa"
-                className="img-fluid"
+                className="img-fluid my-3"
               />
               <Card.Body className="d-flex flex-column">
                 <Card.Title className="arapey fs-1">El Cielo</Card.Title>
@@ -77,11 +92,11 @@ const Canciones = () => {
                 </Card.Text>
               </Card.Body>
             </Card>
-            <Card className="card-canciones d-flex flex-column shadow">
+            <Card className="d-flex flex-column shadow col-12 col-md-5">
               <Card.Img
                 src={porLasNochesTapa}
                 alt="Por las Noches Arte de tapa"
-                className="img-fluid"
+                className="img-fluid my-3"
               />
               <Card.Body className="d-flex flex-column">
                 <Card.Title className="arapey fs-1">Por las Noches</Card.Title>
@@ -96,11 +111,11 @@ const Canciones = () => {
                 </Card.Text>
               </Card.Body>
             </Card>
-            <Card className="card-canciones d-flex flex-column shadow">
+            <Card className="d-flex flex-column shadow col-12 col-md-5">
               <Card.Img
                 src={paraLlevarTapa}
                 alt="Para Llevar Arte de tapa"
-                className="img-fluid"
+                className="img-fluid my-3"
               />
               <Card.Body className="d-flex flex-column">
                 <Card.Title className="arapey fs-1">Para Llevar</Card.Title>
